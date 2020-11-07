a29193a2b722d9c0934eb08bea9f90270aab5dcb
liaolishuai <1402675955@qq.com>
2020-11-07 15:42:42 +0800

    添加了学生类v2.0

---
+++ cloud-eureka-server/src/main/java/com/lxy/entity/student.java
@@ -0,0 +1,14 @@
+package com.lxy.entity;
+
+/**
+ * author:lxy
+ * 类作用：
+ * 创建时间:2020/11/7
+ */
+
+public class student {
+    private String id;
+    private String name;
+    private char sex;
+
+}
